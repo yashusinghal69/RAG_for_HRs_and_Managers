c242a9bd81ce23c11a6e2e0b3b1707039a311001
yashuSinghal <125557043+yashusinghal69@users.noreply.github.com>
2025-08-13 01:55:25 +0530

    issue_solved

--- frontend/src/hooks/use-local-storage.ts
+++ frontend/src/hooks/use-local-storage.ts
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
-  // State to store our value
+ 
   const [storedValue, setStoredValue] = useState<T>(initialValue);
   const [isLoading, setIsLoading] = useState(true);
 
-  // Get value from localStorage on mount
   useEffect(() => {
     try {
       if (typeof window !== "undefined") {
@@ -21,17 +20,13 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     }
   }, [key]);
 
-  // Return a wrapped version of useState's setter function that persists the new value to localStorage
   const setValue = (value: T | ((val: T) => T)) => {
     try {
-      // Allow value to be a function so we have the same API as useState
       const valueToStore =
         value instanceof Function ? value(storedValue) : value;
 
-      // Save state
       setStoredValue(valueToStore);
 
-      // Save to localStorage
       if (typeof window !== "undefined") {
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
       }
